ffb4c17f4df40a5f4ca1f6666bc9d8ce99767777
Oscar Jiang <pengj0520@gmail.com>
2025-04-03 15:11:11 +0800

    Update to 0.1.3

--- mdscraper/__init__.py
+++ mdscraper/__init__.py
@@ -2,7 +2,7 @@
 MDScraper - A tool to fetch webpages and convert their content to clean Markdown format
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.3"
 
 from mdscraper.core.scraper import (
     fetch_content,
